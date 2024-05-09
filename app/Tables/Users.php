1be5daa94da2e7d36d071265a486d603ef927a1c
4th-avenue <quebec706@gmail.com>
2024-05-09 14:14:07 +0900

    SpladeTable 클래스를 이용해 Users의 Index 페이지 표시하기

--- app/Tables/Users.php
+++ app/Tables/Users.php
@@ -4,8 +4,11 @@ namespace App\Tables;
 
 use App\Models\User;
 use Illuminate\Http\Request;
-use ProtoneMedia\Splade\AbstractTable;
+use Illuminate\Support\Collection;
 use ProtoneMedia\Splade\SpladeTable;
+use Spatie\QueryBuilder\QueryBuilder;
+use ProtoneMedia\Splade\AbstractTable;
+use Spatie\QueryBuilder\AllowedFilter;
 
 class Users extends AbstractTable
 {
@@ -36,7 +39,22 @@ class Users extends AbstractTable
      */
     public function for()
     {
-        return User::query();
+        $globalSearch = AllowedFilter::callback('global', function ($query, $value) {
+            $query->where(function ($query) use ($value) {
+                Collection::wrap($value)->each(function ($value) use ($query) {
+                    $query
+                        ->orWhere('username', 'LIKE', "%{$value}%")
+                        ->orWhere('first_name', 'LIKE', "%{$value}%")
+                        ->orWhere('last_name', 'LIKE', "%{$value}%")
+                        ->orWhere('email', 'LIKE', "%{$value}%");
+                });
+            });
+        });
+
+        return QueryBuilder::for(User::class)
+            ->defaultSort('username')
+            ->allowedSorts(['id', 'username', 'first_name', 'last_name', 'email'])
+            ->allowedFilters(['username', 'first_name', 'last_name', 'email', $globalSearch]);
     }
 
     /**
@@ -48,14 +66,12 @@ class Users extends AbstractTable
     public function configure(SpladeTable $table)
     {
         $table
-            ->withGlobalSearch(columns: ['id'])
-            ->column('id', sortable: true);
-
-            // ->searchInput()
-            // ->selectFilter()
-            // ->withGlobalSearch()
-
-            // ->bulkAction()
-            // ->export()
+            ->withGlobalSearch(columns: ['id', 'username', 'first_name', 'last_name', 'email'])
+            ->column('id', sortable: true)
+            ->column('username', sortable: true)
+            ->column('first_name', sortable: true)
+            ->column('last_name', sortable: true)
+            ->column('email', sortable: true)
+            ->paginate(15);
     }
 }
