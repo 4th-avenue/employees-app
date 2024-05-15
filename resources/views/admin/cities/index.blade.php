731a60fc1aa26036764c797e8f561e157538621d
4th-avenue <quebec706@gmail.com>
2024-05-15 10:18:55 +0900

    Cities의 테이블 클래스 생성하고 Index 페이지 표시하기

---
+++ resources/views/admin/cities/index.blade.php
@@ -0,0 +1,24 @@
+<x-admin-layout>
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 bg-white border-b border-gray-200">
+                    <div class="flex justify-between">
+                        <h1 class="mb-4 font-semibold">Cities Index</h1>
+                        <div>
+                            <Link href="{{ route('admin.cities.create') }}" class="px-4 py-2 bg-indigo-500 hover:bg-indigo-700 rounded-md text-white">New City</Link>
+                        </div>
+                    </div>
+                    <x-splade-table :for="$cities">
+                        @cell('action', $city)
+                        <div class="space-x-2">
+                            <Link href="{{ route('admin.cities.edit', $city->id) }}" class="text-green-400 hover:text-green-600 font-semibold">Edit</Link>
+                            <Link confirm="이 지역을 삭제하시겠습니까?" confirm-text="Are you sure?" confirm-button="Yes" cancel-button="No" href="{{ route('admin.cities.destroy', $city->id) }}" method="DELETE" class="text-red-400 hover:text-red-600 font-semibold">Delete</Link>
+                        </div>
+                        @endcell
+                    </x-splade-table>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-admin-layout>
