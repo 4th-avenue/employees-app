60778a216741698c01211a9174bb6ea36d221974
4th-avenue <quebec706@gmail.com>
2024-05-20 11:13:26 +0900

    새 Role 등록 기능 구현

--- app/Http/Controllers/RoleController.php
+++ app/Http/Controllers/RoleController.php
@@ -4,6 +4,12 @@ namespace App\Http\Controllers;
 
 use App\Tables\Roles;
 use Illuminate\Http\Request;
+use Spatie\Permission\Models\Role;
+use ProtoneMedia\Splade\SpladeForm;
+use App\Http\Requests\RoleStoreRequest;
+use ProtoneMedia\Splade\Facades\Splade;
+use ProtoneMedia\Splade\FormBuilder\Input;
+use ProtoneMedia\Splade\FormBuilder\Submit;
 
 class RoleController extends Controller
 {
@@ -12,5 +18,28 @@ class RoleController extends Controller
         return view('admin.roles.index', [
             'roles' => Roles::class
         ]);
+    }
+
+    public function create()
+    {
+        $form = SpladeForm::make()
+            ->action(route('admin.roles.store'))
+            ->fields([
+                Input::make('name')->label('Name'),
+                Submit::make()->label('Save'),
+            ])
+            ->class('space-y-4');
+
+        return view('admin.roles.create', [
+            'form' => $form,
+        ]);
+    }
+
+    public function store(RoleStoreRequest $request)
+    {
+        Role::create($request->validated());
+        Splade::toast('새 역할을 저장했습니다.')->autoDismiss(3);
+
+        return redirect()->route('admin.roles.index');
     }
 }
